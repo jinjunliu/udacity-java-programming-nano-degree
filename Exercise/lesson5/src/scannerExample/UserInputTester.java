bdc23f3146fdb695fb2523bdc587e44c40847d41
Jinjun Liu <ljj7129@gmail.com>
2021-10-13 15:40:56 -0500

    added exercise scannerExample

---
+++ Exercise/lesson5/src/scannerExample/UserInputTester.java
@@ -0,0 +1,19 @@
+package scannerExample;
+
+import java.util.Scanner;
+
+public class UserInputTester {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter a string");
+            String userInput = scanner.nextLine();
+            System.out.println("User input: " + userInput);
+
+        } catch (Exception ex) {
+            ex.getLocalizedMessage();
+        } finally {
+            scanner.close();
+        }
+    }
+}
