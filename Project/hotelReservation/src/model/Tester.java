27567642b654d454ec1b7099b1cb8e79512ec035
Jinjun Liu <ljj7129@gmail.com>
2022-01-18 00:33:34 -0600

    Create Tester.java in model

---
+++ Project/hotelReservation/src/model/Tester.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Tester {
+    public static void main(String[] args) {
+        Customer customer = new Customer("first", "second", "j@domain.com");
+        System.out.println(customer);
+        Customer customer2 = new Customer("first", "second", "email");
+        System.out.println(customer2);
+    }
+}
