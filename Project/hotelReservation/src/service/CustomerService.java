63657d6bde3b3260f385c53131c8094a655e9073
Jinjun Liu <ljj7129@gmail.com>
2022-01-18 14:50:40 -0600

    Update CustomerService.java

--- Project/hotelReservation/src/service/CustomerService.java
+++ Project/hotelReservation/src/service/CustomerService.java
@@ -1,9 +1,22 @@
 package service;
 
-public class CustomerService {
-    public static void addCustomer(String email, String firstName, String lastName) {
+import model.Customer;
+import java.util.*;
 
+public class CustomerService {
+    List<Customer> listOfCustomer = new ArrayList<Customer>();
+    Map<String, Customer> mapOfCustomer = new HashMap<>();
+    public void addCustomer(String email, String firstName, String lastName) {
+        Customer customer = new Customer(firstName, lastName, email);
+        this.listOfCustomer.add(customer);
+        this.mapOfCustomer.put(email, customer);
     }
 
+    public Customer getCustomer(String customerEmail) {
+        return this.mapOfCustomer.get(customerEmail);
+    }
 
+    public Collection<Customer> getAllCustomers() {
+        return this.listOfCustomer;
+    }
 }
