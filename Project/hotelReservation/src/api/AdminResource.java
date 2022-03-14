8b18d34f685a4e1f45b1bffa4f3a90b2da62258d
Jinjun Liu <ljj7129@gmail.com>
2022-03-13 21:58:16 -0500

    Create AdminResource.java

---
+++ Project/hotelReservation/src/api/AdminResource.java
@@ -0,0 +1,28 @@
+package api;
+
+import model.*;
+import java.util.*;
+import service.*;
+
+public class AdminResource {
+    public static Customer getCustomer(String email) {
+        // TODO
+    }
+
+    public static void addRoom(List<IRoom> rooms) {
+        // TODO
+    }
+
+    public static Collection<IRoom> getAllRooms() {
+        // TODO
+    }
+
+    public static Collection<Customer> getAllCustomers() {
+        // TODO
+    }
+
+    public static void displayAllReservations() {
+        // TODO
+    }
+
+}
