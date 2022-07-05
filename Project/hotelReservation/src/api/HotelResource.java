26a50bf611d17ec86b7366079f69b1c6ddb7773f
Jinjun Liu <ljj7129@gmail.com>
2022-07-05 16:35:05 -0500

    Update HotelResource.java

--- Project/hotelReservation/src/api/HotelResource.java
+++ Project/hotelReservation/src/api/HotelResource.java
@@ -5,30 +5,31 @@ import service.*;
 import java.util.*;
 
 public class HotelResource {
+    private static final CustomerService customerService = new CustomerService();
+    private static final ReservationService reservationService = new ReservationService();
     public static Customer getCustomer(String email) {
-        CustomerService customerService = new CustomerService();
         return customerService.getCustomer(email);
     }
 
     public static void createACustomer(String email, String firstName, String lastName) {
-        CustomerService customerService = new CustomerService();
         customerService.addCustomer(firstName, lastName, email);
     }
 
     public IRoom getRoom(String roomNumber) {
-        ReservationService reservationService = new ReservationService();
         return reservationService.getARoom(roomNumber);
     }
 
     public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date checkOutDate) {
-        // TODO
+        Customer customer = customerService.getCustomer(customerEmail);
+        return reservationService.reserveARoom(customer, room, checkInDate, checkOutDate);
     }
 
     public Collection<Reservation> getCustomerReservations(String customerEmail) {
-        // TODO
+        Customer customer = customerService.getCustomer(customerEmail);
+        return reservationService.getCustomersReservation(customer);
     }
 
     public Collection<IRoom> findARoom(Date checkIn, Date checkOut) {
-        // TODO
+        return reservationService.findRooms(checkIn, checkOut);
     }
 }
