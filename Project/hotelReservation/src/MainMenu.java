1ad8efc44c45a163368d7e77c0a8b48ad3ce1853
Jinjun Liu <ljj7129@gmail.com>
2022-07-05 19:09:54 -0500

    Update MainMenu.java

--- Project/hotelReservation/src/MainMenu.java
+++ Project/hotelReservation/src/MainMenu.java
@@ -1,6 +1,4 @@
 import api.*;
-import model.*;
-import service.*;
 import java.util.*;
 
 
@@ -16,13 +14,64 @@ public class MainMenu {
                 5. Exit.
                 -------------------------------
                 Please select a number for the menu option""");
-        Scanner scanner = new Scanner(System.in);
-        try {
-            System.out.println("Enter a string");
-            String userInput = scanner.nextLine();
-            System.out.println("User input: " + userInput);
-        } catch (Exception ex) {
-            ex.getLocalizedMessage();
+        int menuOption = 0;
+        while (true) {
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                try {
+                    menuOption = scanner.nextInt();
+                    if (1 <= menuOption && menuOption <= 5) {
+                        System.out.println("You selected menu option " + menuOption);
+                    }
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Please enter a valid number (1-5) for the menu option");
+                }
+            }
+            switch (menuOption) {
+                case 1 -> findAndReserveARoom();
+                case 2 -> seeMyReservations();
+                case 3 -> createAnAccount();
+                case 4 -> admin();
+                case 5 -> {
+                    System.out.println("Thank you for using our application!");
+                    System.exit(0);
+                }
+                default -> System.out.println("Please enter a valid number (1-5) for the menu option");
+            }
+        }
+    }
+
+    private static void findAndReserveARoom() {
+        System.out.println("Please enter your check in date (yyyy-mm-dd):");
+        int menuOption = 0;
+        while (true) {
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                try {
+                    menuOption = scanner.nextInt();
+                    if (1 <= menuOption && menuOption <= 5) {
+                        System.out.println("You selected menu option " + menuOption);
+                    }
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Please enter a valid number (1-5) for the menu option");
+                }
+            }
+            switch (menuOption) {
+                case 1 -> findARoom();
+                case 2 -> seeAllRooms();
+                case 3 -> seeAllReservations();
+                case 4 -> {
+                    System.out.println("Back to main menu");
+                    return;
+                }
+                case 5 -> {
+                    System.out.println("Thank you for using our application!");
+                    System.exit(0);
+                }
+                default -> System.out.println("Please enter a valid number (1-5) for the menu option");
+            }
         }
     }
 }
