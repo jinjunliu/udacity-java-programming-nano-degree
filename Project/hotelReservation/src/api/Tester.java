c9535c90ad2ea3ed0d01d375d271c3e2ad399970
Jinjun Liu <ljj7129@gmail.com>
2022-07-09 22:47:07 -0500

    Create Tester.java

---
+++ Project/hotelReservation/src/api/Tester.java
@@ -0,0 +1,9 @@
+package api;
+
+public class Tester {
+    public static void main(String[] args) {
+        HotelResource hotelResource = new HotelResource();
+        AdminResource adminResource = new AdminResource();
+        System.out.println("Welcome to hotel reservation application!");
+    }
+}
