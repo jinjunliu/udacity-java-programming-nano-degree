1bc58bb029256b8df77d00ddc14a60d3b16d7420
Jinjun Liu <ljj7129@gmail.com>
2022-07-11 02:25:23 -0500

    Update HotelApplication.java

--- Project/hotelReservation/src/HotelApplication.java
+++ Project/hotelReservation/src/HotelApplication.java
@@ -1,5 +1,6 @@
 public class HotelApplication {
     public static void main(String[] args) {
+        System.out.println("Welcome to hotel reservation application!");
         MainMenu.mainMenu();
     }
 }
