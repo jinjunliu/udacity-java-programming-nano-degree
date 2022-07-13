f181221d3f3355d56aa529bb2fada7f7d00e8b8b
Jinjun Liu <ljj7129@gmail.com>
2022-07-13 09:27:56 -0500

    Update Room.java

--- Project/hotelReservation/src/model/Room.java
+++ Project/hotelReservation/src/model/Room.java
@@ -19,6 +19,33 @@ public class Room implements IRoom {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Room other = (Room) obj;
+        if (!this.roomNumber.equals(other.roomNumber)) {
+            return false;
+        }
+        if (!this.price.equals(other.price)) {
+            return false;
+        }
+        return this.roomType == other.roomType;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 13 * hash + (this.roomNumber != null ? this.roomNumber.hashCode() : 0);
+        hash = 13 * hash + (this.price != null ? this.price.hashCode() : 0);
+        hash = 13 * hash + (this.roomType != null ? this.roomType.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
     public String getRoomNumber() {
         return this.roomNumber;
     }
