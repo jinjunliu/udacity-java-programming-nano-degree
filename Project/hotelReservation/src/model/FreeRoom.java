41379b6e011daced079d44fccb0b44f9a4909af6
Jinjun Liu <ljj7129@gmail.com>
2022-07-13 22:40:05 -0500

    Update FreeRoom.java

--- Project/hotelReservation/src/model/FreeRoom.java
+++ Project/hotelReservation/src/model/FreeRoom.java
@@ -3,11 +3,11 @@ package model;
 public class FreeRoom extends Room {
     public FreeRoom(String roomNumber, Double price, RoomType roomType) {
         super(roomNumber, price, roomType);
-        this.price = 0.0;
+        this.setRoomPrice(0.0);
     }
 
     @Override
     public String toString() {
-        return "Room number: " + roomNumber + "\nRoom type: " + roomType.toString() + "\nThis is a free room.";
+        return "Room number: " + this.getRoomNumber() + "\nRoom type: " + this.getRoomType().toString() + "\nThis is a free room.";
     }
 }
