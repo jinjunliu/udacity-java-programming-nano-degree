9ea9fe23ab0eff9fc51121759947bb977614c279
Jinjun Liu <ljj7129@gmail.com>
2022-07-15 18:31:09 -0500

    Update App.java

--- Exercise/Advanced_Java_Programming/defineFunctionalInterface/src/App.java
+++ Exercise/Advanced_Java_Programming/defineFunctionalInterface/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         BinaryOperation add = (a, b) -> a + b;
+        System.out.println(add.apply(1, 2));
         assert 5 == add.apply(2, 3);
     }
 }
