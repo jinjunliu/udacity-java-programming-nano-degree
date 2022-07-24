2ba0febcf6a2b4bfc7cca04033f3f004bf07cda1
Jinjun Liu <ljj7129@gmail.com>
2022-07-23 19:30:56 -0500

    Create builderExcercise

---
+++ Exercise/Advanced_Java_Programming/builderExcercise/src/Main.java
@@ -0,0 +1,22 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+
+public final class Main {
+
+    public static void main(String[] args) {
+        UdacisearchClient client =
+                new UdacisearchClient.Builder()
+                        .setName("CatFacts LLC")
+                        .setId(17)
+                        .setQuarterlyBudget(8000)
+                        .setNumEmployees(5)
+                        .setContractStart(Instant.now())
+                        .setContractLength(Duration.ofDays(180))
+                        .setTimeZone(ZoneId.of("America/Los_Angeles"))
+                        .setBillingAddress("555 Meowmers Ln, Riverside, CA 92501")
+                        .build();
+
+        System.out.println(client);
+    }
+}
