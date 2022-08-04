cab892509a6db237d26708687a55d9ab3daadd9d
Jinjun Liu <ljj7129@gmail.com>
2022-08-04 00:04:20 -0500

    Create threadsExercise

---
+++ Exercise/Advanced_Java_Programming/threadsExcercise/src/IntPrinter.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public final class IntPrinter {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: Main <number of threads>");
+            return;
+        }
+
+        int n = Integer.parseInt(args[0]);
+
+        // TODO: Create a list of n Threads and run them all in parallel with the System.out.println
+        //       statement.
+        List<Thread> threads = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            int finalI = i;
+            threads.add(new Thread(new IntRunner(finalI)));
+        }
+        for (Thread t: threads) {
+            t.start();
+        }
+        for (Thread t: threads) {
+            t.join();
+        }
+    }
+
+    private record IntRunner(int value) implements Runnable {
+
+        @Override
+            public void run() {
+                System.out.println("Thread #" + Thread.currentThread().getId() + " printed " + value);
+            }
+        }
+
+}
