b4cb34f7cbd903e0ab54fdfdcc3a1a109b8c6afc
Jinjun Liu <ljj7129@gmail.com>
2022-08-10 23:42:16 -0500

    Reformat ConfigurationLoader.java

--- Project/webCrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
+++ Project/webCrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
@@ -9,37 +9,37 @@ import java.util.Objects;
  */
 public final class ConfigurationLoader {
 
-  private final Path path;
+    private final Path path;
 
-  /**
-   * Create a {@link ConfigurationLoader} that loads configuration from the given {@link Path}.
-   */
-  public ConfigurationLoader(Path path) {
-    this.path = Objects.requireNonNull(path);
-  }
+    /**
+     * Create a {@link ConfigurationLoader} that loads configuration from the given {@link Path}.
+     */
+    public ConfigurationLoader(Path path) {
+        this.path = Objects.requireNonNull(path);
+    }
 
-  /**
-   * Loads configuration from this {@link ConfigurationLoader}'s path
-   *
-   * @return the loaded {@link CrawlerConfiguration}.
-   */
-  public CrawlerConfiguration load() {
-    // TODO: Fill in this method.
+    /**
+     * Loads configuration from this {@link ConfigurationLoader}'s path
+     *
+     * @return the loaded {@link CrawlerConfiguration}.
+     */
+    public CrawlerConfiguration load() {
+        // TODO: Fill in this method.
 
-    return new CrawlerConfiguration.Builder().build();
-  }
+        return new CrawlerConfiguration.Builder().build();
+    }
 
-  /**
-   * Loads crawler configuration from the given reader.
-   *
-   * @param reader a Reader pointing to a JSON string that contains crawler configuration.
-   * @return a crawler configuration
-   */
-  public static CrawlerConfiguration read(Reader reader) {
-    // This is here to get rid of the unused variable warning.
-    Objects.requireNonNull(reader);
-    // TODO: Fill in this method
+    /**
+     * Loads crawler configuration from the given reader.
+     *
+     * @param reader a Reader pointing to a JSON string that contains crawler configuration.
+     * @return a crawler configuration
+     */
+    public static CrawlerConfiguration read(Reader reader) {
+        // This is here to get rid of the unused variable warning.
+        Objects.requireNonNull(reader);
+        // TODO: Fill in this method
 
-    return new CrawlerConfiguration.Builder().build();
-  }
+        return new CrawlerConfiguration.Builder().build();
+    }
 }
