0743fcda77cd1f3500cf4e134e62ac715d7bffb8
Jinjun Liu <ljj7129@gmail.com>
2022-08-12 03:22:15 -0500

    Update WebCrawlerMain.java

--- Project/webCrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ Project/webCrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -37,6 +37,12 @@ public final class WebCrawlerMain {
         CrawlResult result = crawler.crawl(config.getStartPages());
         CrawlResultWriter resultWriter = new CrawlResultWriter(result);
         // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
+        String resultPath = config.getResultPath();
+        if (resultPath.isEmpty()) {
+            resultWriter.write(new OutputStreamWriter(System.out));
+        } else {
+            resultWriter.write(Path.of(resultPath));
+        }
         // TODO: Write the profile data to a text file (or System.out if the file name is empty)
     }
 
