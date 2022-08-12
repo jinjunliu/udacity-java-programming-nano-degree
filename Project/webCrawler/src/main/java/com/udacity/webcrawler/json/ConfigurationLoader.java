2d9ceb2e3378a9cc76c04c20eef147e4ca713599
Jinjun Liu <ljj7129@gmail.com>
2022-08-12 02:29:25 -0500

    Update ConfigurationLoader.java

--- Project/webCrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
+++ Project/webCrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
@@ -2,8 +2,11 @@ package com.udacity.webcrawler.json;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
+
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.json.*;
 
 /**
@@ -26,9 +29,12 @@ public final class ConfigurationLoader {
      * @return the loaded {@link CrawlerConfiguration}.
      */
     public CrawlerConfiguration load() {
-        // TODO: Fill in this method.
-
-        return new CrawlerConfiguration.Builder().build();
+        // read the JSON string from path
+        try (Reader reader = Files.newBufferedReader(path)) {
+            return read(reader);
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to read configuration from " + path, e);
+        }
     }
 
     /**
@@ -40,10 +46,10 @@ public final class ConfigurationLoader {
     public static CrawlerConfiguration read(Reader reader) {
         // This is here to get rid of the unused variable warning.
         Objects.requireNonNull(reader);
-        // TODO: Fill in this method
         // read the JSON input and parse it into a CrawlerConfiguration using the Jackson JSON library
         // ref: https://www.baeldung.com/jackson-object-mapper-tutorial
         JsonMapper mapper = new JsonMapper();
+        mapper.disable(JsonParser.Feature.AUTO_CLOSE_SOURCE);
         try {
             return mapper.readValue(reader, CrawlerConfiguration.class);
         } catch (IOException e) {
