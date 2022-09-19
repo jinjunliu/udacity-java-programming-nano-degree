a2925f192620ce60291c962643e20c036936388c
Jinjun Liu <ljj7129@gmail.com>
2022-09-19 15:10:38 -0500

    Update ProfilingMethodInterceptor.java

--- Project/webCrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ Project/webCrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -3,6 +3,9 @@ package com.udacity.webcrawler.profiler;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
 /**
@@ -11,20 +14,33 @@ import java.util.Objects;
  */
 final class ProfilingMethodInterceptor implements InvocationHandler {
 
-  private final Clock clock;
+    private final Clock clock;
+    private final Object delegate;
+    private final ProfilingState state;
+    private final ZonedDateTime startTime;
 
-  // TODO: You will need to add more instance fields and constructor arguments to this class.
-  ProfilingMethodInterceptor(Clock clock) {
+  ProfilingMethodInterceptor(Clock clock, Object delegate, ProfilingState state, ZonedDateTime startTime) {
     this.clock = Objects.requireNonNull(clock);
+    this.delegate = Objects.requireNonNull(delegate);
+    this.state = Objects.requireNonNull(state);
+    this.startTime = Objects.requireNonNull(startTime);
   }
 
   @Override
   public Object invoke(Object proxy, Method method, Object[] args) {
-    // TODO: This method interceptor should inspect the called method to see if it is a profiled
-    //       method. For profiled methods, the interceptor should record the start time, then
-    //       invoke the method using the object that is being profiled. Finally, for profiled
-    //       methods, the interceptor should record how long the method call took, using the
-    //       ProfilingState methods.
-    return null;
+    Object result = null;
+    if (method.isAnnotationPresent(Profiled.class)) {
+      Instant start = clock.instant();
+      try {
+        result = method.invoke(delegate, args);
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        Instant end = clock.instant();
+        Duration duration = Duration.between(start, end);
+        state.record(delegate.getClass(), method, duration);
+      }
+    }
+    return result;
   }
 }
