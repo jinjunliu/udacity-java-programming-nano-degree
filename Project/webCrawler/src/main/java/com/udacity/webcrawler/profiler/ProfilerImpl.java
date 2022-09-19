cf5caba3aa10b7f1cd5dcbf3b7c2cb414a7410a0
Jinjun Liu <ljj7129@gmail.com>
2022-09-19 18:15:05 -0500

    Update ProfilerImpl.java

--- Project/webCrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ Project/webCrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -3,10 +3,15 @@ package com.udacity.webcrawler.profiler;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Clock;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.lang.reflect.Proxy;
 
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
 
@@ -28,18 +33,36 @@ final class ProfilerImpl implements Profiler {
   @Override
   public <T> T wrap(Class<T> klass, T delegate) {
     Objects.requireNonNull(klass);
+    Objects.requireNonNull(delegate);
 
     // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
     //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
     //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
 
-    return delegate;
+    List<Method> methods = Arrays.asList(klass.getMethods());
+    boolean hasProfiledMethod = methods.stream().anyMatch(method -> method.isAnnotationPresent(Profiled.class));
+
+    if (!hasProfiledMethod) {
+      throw new IllegalArgumentException("No methods annotated with @Profiled");
+    }
+
+    return (T) Proxy.newProxyInstance(
+            ProfilerImpl.class.getClassLoader(),
+            new Class[]{klass},
+            new ProfilingMethodInterceptor(clock, delegate, state));
   }
 
   @Override
   public void writeData(Path path) {
     // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
     //       path, the new data should be appended to the existing file.
+    Objects.requireNonNull(path);
+    try (Writer writer = Files.newBufferedWriter(path)) {
+      writeData(writer);
+      writer.flush();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
