1754f7641e6f95c5ffed666057c4ddd83e0b9676
Jinjun Liu <ljj7129@gmail.com>
2022-10-01 18:30:40 -0500

    Update module-info

--- Exercise/Java_Application_Deployment/moduleTypeExercise/modules/com.udacity.hotel/module-info.java
+++ Exercise/Java_Application_Deployment/moduleTypeExercise/modules/com.udacity.hotel/module-info.java
@@ -1,3 +1,6 @@
 module com.udacity.hotel {
 	// Add your code here
+    exports com.udacity.hotel.search;
+    exports com.udacity.hotel.model;
+    opens com.udacity.hotel.model to com.udacity.packagesearch;
 }
