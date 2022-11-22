894b8c313c80d964630ec09f039c927e3d4bf886
Jinjun Liu <ljj7129@gmail.com>
2022-11-21 18:00:18 -0600

    Update AwsImageService.java

--- Project/UdaSecurity/Image/src/main/java/com/udacity/catpoint/image/service/AwsImageService.java
+++ Project/UdaSecurity/Image/src/main/java/com/udacity/catpoint/image/service/AwsImageService.java
@@ -33,7 +33,7 @@ import java.util.stream.Collectors;
  *      aws.secret=[your Secret access key]
  *      aws.region=[an aws region of choice. For example: us-east-2]
  */
-public class AwsImageService {
+public class AwsImageService implements ImageService {
 
     private Logger log = LoggerFactory.getLogger(AwsImageService.class);
 
@@ -63,10 +63,10 @@ public class AwsImageService {
     /**
      * Returns true if the provided image contains a cat.
      * @param image Image to scan
-     * @param confidenceThreshhold Minimum threshhold to consider for cat. For example, 90.0f would require 90% confidence minimum
+     * @param confidenceThreshold Minimum threshhold to consider for cat. For example, 90.0f would require 90% confidence minimum
      * @return
      */
-    public boolean imageContainsCat(BufferedImage image, float confidenceThreshhold) {
+    public boolean imageContainsCat(BufferedImage image, float confidenceThreshold) {
         Image awsImage = null;
         try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
             ImageIO.write(image, "jpg", os);
@@ -75,7 +75,7 @@ public class AwsImageService {
             log.error("Error building image byte array", ioe);
             return false;
         }
-        DetectLabelsRequest detectLabelsRequest = DetectLabelsRequest.builder().image(awsImage).minConfidence(confidenceThreshhold).build();
+        DetectLabelsRequest detectLabelsRequest = DetectLabelsRequest.builder().image(awsImage).minConfidence(confidenceThreshold).build();
         DetectLabelsResponse response = rekognitionClient.detectLabels(detectLabelsRequest);
         logLabelsForFun(response);
         return response.labels().stream().filter(l -> l.name().toLowerCase().contains("cat")).findFirst().isPresent();
