8ae6f83988429e6e27c3ea03241f24fc2801778b
Jinjun Liu <ljj7129@gmail.com>
2023-01-17 18:27:38 -0600

    Update SecurityServiceTest.java

--- Project/UdaSecurity/Security/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
+++ Project/UdaSecurity/Security/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
@@ -1,3 +1,14 @@
+// reference: https://www.digitalocean.com/community/tutorials/mockito-mock-examples
 package com.udacity.catpoint.security.service;
+
+import static org.mockito.Mockito.*;
 public class SecurityServiceTest {
+    public void test1() {
+        // Create a mock object
+        SecurityService securityService = mock(SecurityService.class);
+        // Set the return value of the method
+        when(securityService.getSecurityStatus()).thenReturn("OK");
+        // Verify the return value
+        assertEquals("OK", securityService.getSecurityStatus());
+    }
 }
