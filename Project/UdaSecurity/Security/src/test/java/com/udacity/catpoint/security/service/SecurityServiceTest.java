850c6de27542d23497bcbb8cdfc9b19a3964b265
Jinjun Liu <ljj7129@gmail.com>
2023-01-20 10:30:48 -0600

    Update SecurityServiceTest.java

--- Project/UdaSecurity/Security/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
+++ Project/UdaSecurity/Security/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
@@ -1,8 +1,15 @@
 // reference: https://www.digitalocean.com/community/tutorials/mockito-mock-examples
 package com.udacity.catpoint.security.service;
 
+import com.udacity.catpoint.image.service.ImageService;
+import com.udacity.catpoint.security.service.SecurityService;
+import org.junit.jupiter.api.Test;
+
 import static org.mockito.Mockito.*;
 public class SecurityServiceTest {
+
+    // Test 1: If alarm is armed and a sensor becomes activated, put the system into pending alarm status.
+    @Test
     public void test1() {
         // Create a mock object
         SecurityService securityService = mock(SecurityService.class);
