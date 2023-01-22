c6e5def02698c08a9eb8e1f6a57088af0f9aa2a2
Jinjun Liu <ljj7129@gmail.com>
2023-01-22 17:23:59 -0600

    Update SecurityServiceTest.java

--- Project/UdaSecurity/Security/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
+++ Project/UdaSecurity/Security/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
@@ -13,8 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Set;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class SecurityServiceTest {
@@ -86,7 +85,7 @@ public class SecurityServiceTest {
 
     // 3. If pending alarm and all sensors are inactive, return to no alarm state.
     @ParameterizedTest
-    @EnumSource(value = SensorType.class, names = {"MOTION", "DOOR", "WINDOW"})
+    @EnumSource(SensorType.class)
     public void test3(SensorType sensorType) {
         when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
         sensor.setSensorType(sensorType);
@@ -96,7 +95,39 @@ public class SecurityServiceTest {
         verify(securityRepository).setAlarmStatus(AlarmStatus.NO_ALARM);
     }
 
-    // If alarm is active, change in sensor state should not affect the alarm state.
+    // 4. If alarm is active, change in sensor state should not affect the alarm state.
+    @ParameterizedTest
+    @MethodSource("provideTest4Arguments")
+    public void test4(SensorType sensorType, boolean isActive) {
+        // reference: https://www.baeldung.com/mockito-unnecessary-stubbing-exception
+        lenient().when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.ALARM);
+        sensor.setSensorType(sensorType);
+        sensor.setActive(isActive);
+        // change sensor activation status
+        securityService.changeSensorActivationStatus(sensor, !isActive);
+        verify(securityRepository, never()).setAlarmStatus(any(AlarmStatus.class));
+    }
 
+    private static Object[][] provideTest4Arguments() {
+        return new Object[][] {
+                {SensorType.MOTION, true},
+                {SensorType.MOTION, false},
+                {SensorType.DOOR, true},
+                {SensorType.DOOR, false},
+                {SensorType.WINDOW, true},
+                {SensorType.WINDOW, false}
+        };
+    }
+
+    // 5. If a sensor is activated while already active and the system is in pending state, change it to alarm state.
+    @ParameterizedTest
+    @EnumSource(SensorType.class)
+    public void test5(SensorType sensorType) {
+        sensor.setSensorType(sensorType);
+        sensor.setActive(true);
+        when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
+        securityService.changeSensorActivationStatus(sensor, true);
+        verify(securityRepository).setAlarmStatus(AlarmStatus.ALARM);
+    }
 
 }
